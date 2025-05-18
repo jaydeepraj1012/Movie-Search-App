481ddeff735e4237ab9b5142024c1181cbc6af80
jaydeepraj1012 <jaydeeprajtech@gmail.com>
2025-05-18 14:02:38 +0530

    update image src

--- src/Component/Moviecomponent/MovieListed.jsx
+++ src/Component/Moviecomponent/MovieListed.jsx
@@ -13,7 +13,7 @@ function MovieListed({ movie }) {
       {/* Poster */}
       <div className="w-full h-[500px] overflow-hidden rounded-lg shadow relative">
         <img
-          src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+         src={movie.poster_path ? `https://image.tmdb.org/t/p/w500${movie.poster_path}` : "https://via.placeholder.com/300x450?text=No+Image"}
           alt={movie.title}
           className="w-full h-full object-cover object-center"
           onError={(e) => {
